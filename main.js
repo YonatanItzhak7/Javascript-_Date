eddf6e5981391d8b1793effa6d4cbb36336b8810
Yonatanitzhak7 <Yonatan1171@gmail.com>
2022-05-24 09:59:55 +0300

    commit

---
+++ main.js
@@ -0,0 +1,102 @@
+// function printDate (){
+//     var myDate = new Date();
+//     console.log(myDate);
+// }
+// printDate()
+
+// function printStrToDate(myDateStr){
+//     var myDate = new Date(myDateStr);
+//     console.log(myDate);
+// }
+// printStrToDate("9/29/1997")
+
+// function printDate(){
+//   var myDate = new Date(prompt("Enter Date"))
+//   console.log(myDate);
+// }
+// printDate()
+
+// function printMonth(){
+//     var myDate = new Date(prompt("Enter Date"))
+//     myDate.getMonth()
+//     return myDate
+// }
+// printMonth()
+
+// function printHour(){
+//     var myDate = new Date(prompt("Enter Date"))
+//     myDate.getHours()
+//     return myDate
+// }
+// printHour()
+
+// function printYear(){
+//     var myDate = new Date(prompt("Enter Date"))
+//     myDate.getFullYear()
+//     return myDate
+// }
+// printYear()
+
+// function printMinutes(){
+//     var myDate = new Date(prompt("Enter Date"))
+//     myDate.getMinutes()
+//     console.log(myDate);
+// }
+// printMinutes()
+
+// function printTheEarlyDate(){
+//     var myDateOne = new Date(prompt("Enter Date"))
+//     var myDateTwo = new Date(prompt("Enter Date"))
+//     if(myDateOne > myDateTwo){
+//         return myDateOne
+//     }
+//     else{
+//         return myDateTwo;
+//     }
+// }
+// printTheEarlyDate()
+
+// function printAge(){
+//     var myBirthDay = new Date(prompt("Enter your birthday"))
+//     var myNowYear = new Date(prompt("Enter the current date "))
+//     var age = myNowYear.getFullYear() - myBirthDay.getFullYear()
+//     console.log(age);
+// }
+// printAge()
+
+// function printRndDate() {
+//   var myDate = new Date(prompt("Enter birth date"));
+//   var myYear = new Date();
+//   var myAge =  myYear.getFullYear()-myDate.getFullYear();
+//   if (myAge >=18) {
+//     alert("Age accepted, may continue");
+//   }
+// else{
+//   alert("Sorry but the age was not confirmed");
+// }
+// }
+
+// printRndDate();
+
+// function printRndDate(){
+//     var rndYear = Math.floor(Math.random()*(2022-1990)+1990);
+//     var rndMonth = Math.floor(Math.random()*(12-1)+1);
+//     var rndDay = Math.floor(Math.random()*(31 - 1)+1);
+//     var myDate = rndMonth + "/" + rndDay +"/"+rndYear;
+//     console.log(myDate);
+// }
+// printRndDate()
+
+
+
+
+
+
+// -------------------One before the last---------------------
+// function printLocation(){
+//     var myDate = new Date();
+//     console.log(myDate);
+
+// }
+// printLocation()
+// -------------------One before the last---------------------
